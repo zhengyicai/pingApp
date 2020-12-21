1a496918967d62b73b2438a1f0112fe565a23e02
zhengyc <1010172623@qq.com>
2020-12-22 00:09:37 +0800

    create

---
+++ showApp/js/path.js
@@ -0,0 +1,8 @@
+var  $requestUrl = "http://device.kangxiaoshuai.com/cms/";
+
+//var  $requestUrl = "http://192.168.1.104:8080/cms/";
+
+var  $imageUrl = "http://device.kangxiaoshuai.com:8000/";
+var $device = "";
+
+var $pageSize="";
---
+++ showApp/unpackage/resources/H51EAB305/www/js/path.js
@@ -0,0 +1,7 @@
+var  $requestUrl = "http://device.kangxiaoshuai.com/cms/";
+
+
+var  $imageUrl = "http://device.kangxiaoshuai.com:8000/";
+var $device = "";
+
+var $pageSize="";
